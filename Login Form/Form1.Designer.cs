55e70c824a3073b1cffc886214b55d0efa55ae9a
Marks Sarkisovs <mark.sarkisov@gmail.com>
2024-10-05 22:15:49 +0200

    Add project files.

---
+++ Login Form/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Login_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_c = new System.Windows.Forms.Button();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.txt_box_PinCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.Linen;
+            this.btn_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_1.Location = new System.Drawing.Point(35, 141);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(85, 82);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            this.btn_1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.Color.Linen;
+            this.btn_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_2.Location = new System.Drawing.Point(186, 141);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(85, 82);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = false;
+            this.btn_2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.Color.Linen;
+            this.btn_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_3.Location = new System.Drawing.Point(348, 141);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(85, 82);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = false;
+            this.btn_3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.Color.Linen;
+            this.btn_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_4.Location = new System.Drawing.Point(35, 261);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(85, 82);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = false;
+            this.btn_4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.Color.Linen;
+            this.btn_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_5.Location = new System.Drawing.Point(186, 261);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(85, 82);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = false;
+            this.btn_5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.Color.Linen;
+            this.btn_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_6.Location = new System.Drawing.Point(348, 261);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(85, 82);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = false;
+            this.btn_6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.Color.Linen;
+            this.btn_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_7.Location = new System.Drawing.Point(35, 386);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(85, 82);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = false;
+            this.btn_7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.Color.Linen;
+            this.btn_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_8.Location = new System.Drawing.Point(186, 386);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(85, 82);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = false;
+            this.btn_8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.BackColor = System.Drawing.Color.Linen;
+            this.btn_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_9.Location = new System.Drawing.Point(348, 386);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(85, 82);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = false;
+            this.btn_9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_0
+            // 
+            this.btn_0.BackColor = System.Drawing.Color.Linen;
+            this.btn_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_0.Location = new System.Drawing.Point(186, 497);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(85, 82);
+            this.btn_0.TabIndex = 9;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = false;
+            this.btn_0.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_c
+            // 
+            this.btn_c.BackColor = System.Drawing.Color.Linen;
+            this.btn_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_c.Location = new System.Drawing.Point(35, 497);
+            this.btn_c.Name = "btn_c";
+            this.btn_c.Size = new System.Drawing.Size(85, 82);
+            this.btn_c.TabIndex = 10;
+            this.btn_c.Text = "CE";
+            this.btn_c.UseVisualStyleBackColor = false;
+            this.btn_c.Click += new System.EventHandler(this.btn_c_Click);
+            // 
+            // btn_del
+            // 
+            this.btn_del.BackColor = System.Drawing.Color.Linen;
+            this.btn_del.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_del.Location = new System.Drawing.Point(348, 497);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(85, 82);
+            this.btn_del.TabIndex = 11;
+            this.btn_del.Text = "<";
+            this.btn_del.UseVisualStyleBackColor = false;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // txt_box_PinCode
+            // 
+            this.txt_box_PinCode.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txt_box_PinCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_box_PinCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt_box_PinCode.Location = new System.Drawing.Point(76, 25);
+            this.txt_box_PinCode.MaxLength = 4;
+            this.txt_box_PinCode.Multiline = true;
+            this.txt_box_PinCode.Name = "txt_box_PinCode";
+            this.txt_box_PinCode.Size = new System.Drawing.Size(309, 95);
+            this.txt_box_PinCode.TabIndex = 1;
+            this.txt_box_PinCode.TextChanged += new System.EventHandler(this.txt_box_PinCode_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(481, 613);
+            this.Controls.Add(this.txt_box_PinCode);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.btn_c);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_c;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.TextBox txt_box_PinCode;
+    }
+}
+
---
+++ Login Form/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_Form
+{
+    public partial class Form1 : Form
+    {
+        double resultValue = 0;
+        string operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=CHERNAYADIRA\SQLEXPRESS;Initial Catalog=""Time Register"";Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //txt_box_PinCode.PasswordChar = '*';
+            txt_box_PinCode.MaxLength = 4;
+
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (txt_box_PinCode.Text == "0" || (isOperationPerformed))
+                txt_box_PinCode.Clear();
+
+            isOperationPerformed = false;
+
+            Button button = (Button)sender;
+
+            //validation on deciamls more than 1
+            if (button.Text == ".")
+            {
+                if (!txt_box_PinCode.Text.Contains("."))
+                    txt_box_PinCode.Text += button.Text;
+            }
+            else
+            {
+                txt_box_PinCode.Text += button.Text;
+            }
+        }
+
+        private void btn_c_Click(object sender, EventArgs e)
+        {
+            txt_box_PinCode.Text = "0";
+            resultValue = 0;
+        }
+
+        private void btn_del_Click(object sender, EventArgs e)
+        {
+            if (txt_box_PinCode.Text.Length > 0)
+                txt_box_PinCode.Text = txt_box_PinCode.Text.Remove(txt_box_PinCode.Text.Length - 1, 1);
+
+            if (txt_box_PinCode.Text == "")
+                txt_box_PinCode.Text = "0";
+        }
+
+        private void txt_box_PinCode_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (txt_box_PinCode.Text.Length>4)
+            {
+                MessageBox.Show("The maximum limit is 4");
+            }
+        }
+    }
+}
